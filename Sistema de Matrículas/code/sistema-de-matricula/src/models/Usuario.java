08c0912e6718d1d8bcb604fa0f3e962f8d21342a
raphael sena <rsenares1@gmail.com>
2025-02-18 14:26:45 -0300

    feat: add class Usuario

---
+++ Sistema de Matrículas/code/sistema-de-matricula/src/models/Usuario.java
@@ -0,0 +1,55 @@
+package models;
+
+import java.util.Objects;
+
+public class Usuario {
+    private Integer id;
+    private String nome;
+    private String senha;
+
+    public Usuario() {
+    }
+
+    public Usuario(Integer id, String nome, String senha) {
+        this.id = id;
+        this.nome = nome;
+        this.senha = senha;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Usuario usuario = (Usuario) o;
+        return Objects.equals(id, usuario.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
